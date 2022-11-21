4f86ab04b8d47c6fedbba205d0410d40585b8fec
Ali <dev@macs-MacBook-Pro.local>
2022-11-21 15:01:15 +0500

    Added unique on name in restaurant.

--- restaurants/models/restaurant.py
+++ restaurants/models/restaurant.py
@@ -5,7 +5,7 @@ from customers.models.customer import Customer
 
 class Restaurant(models.Model):
     owner = models.ForeignKey(Customer, on_delete=models.CASCADE,default=1)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50,unique=True)
     location = models.CharField(max_length=50)
     contact = PhoneNumberField()
 
